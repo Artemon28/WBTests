82fae4c8be706ac9d78725df951a24cd74785773
Artemiy <los28.2001@mail.ru>
2022-08-01 19:49:48 +0300

    15 done

--- l1/15.go
+++ l1/15.go
@@ -3,21 +3,43 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
+	"reflect"
+	"unsafe"
 )
 
 var justString string
 
+//Проблема - у нас есть длинная строка, которая занимает много памяти. Когда мы делаем срез этой строки, мы не
+//копируем его, а ссылаемся на изначальную строку. Таким образом в памяти хранится вся изначальная строка
+// и сборщик мусора не освобождает, даже когда мы ей, не пользуемся, например если мы вернём эту строку из функции.
 func someFunc() {
 	v := createHugeString(1 << 10)
-	//justString не копирует данные, а является указателем на v, то есть, если мы изменим v, то изменится и значение justString.
-	//Однако строки это неизменяемые слайсы битов.
 	justString = v[:100]
-	fmt.Println(&justString)
-	fmt.Println(&v)
-	justString = strings.Replace(justString, justString[:10], "1234567890", -1)
-	fmt.Println(&justString)
-	fmt.Println(&v)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&v)).Data)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&justString)).Data)
+}
+
+func correctSomeFunc1() string {
+	v := createHugeString(1 << 10)
+	justString := v[:100]
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&v)).Data)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&justString)).Data)
+	copyString := make([]byte, len(justString))
+	copy(copyString, justString)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&copyString)).Data)
+	return string(copyString)
+}
+
+//ещё есть проблема со срезом по количеству байт, но наш string может быть массивом rune, тогда мы обрежем руну.
+//Поэтому как вариант решения не копировать строку напрямую, а сделать массив рун,
+//который уже будет ссылаться на другую область памяти
+
+func correctSomeFunc2() string {
+	v := createHugeString(1 << 10)
+	justString := []rune(v)[:100]
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&v)).Data)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&justString)).Data)
+	return string(justString)
 }
 
 func createHugeString(len int) string {
@@ -30,5 +52,7 @@ func createHugeString(len int) string {
 }
 
 func main() {
-	someFunc()
+	correctSomeFunc1()
+	fmt.Println("")
+	correctSomeFunc2()
 }
