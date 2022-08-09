caf130addb6207ef590bd10c9eba13b27c7dc79c
Artemiy <los28.2001@mail.ru>
2022-08-09 12:22:36 +0300

    l1 16, 17 remade

--- l1/16.go
+++ l1/16.go
@@ -3,19 +3,47 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sort"
 )
 
+func quickSort(array []int, l int, r int) {
+	if l < r {
+		q := partition(array, l, r)
+		quickSort(array, l, q)
+		quickSort(array, q+1, r)
+	}
+}
+
+func partition(array []int, l int, r int) int {
+	pivot := array[(l+r)/2]
+	i := l
+	j := r
+	for i <= j {
+		for array[i] < pivot {
+			i++
+		}
+		for array[j] > pivot {
+			j--
+		}
+		if i >= j {
+			break
+		}
+		array[i], array[j] = array[j], array[i]
+		i++
+		j--
+	}
+	return j
+
+}
+
 func main() {
-	array := make([]int, 100)
-	for i := 0; i < 100; i++ {
-		array[i] = rand.Intn(1000)
+	array := make([]int, 0, 100)
+	for i := 0; i < 10; i++ {
+		array = append(array, rand.Intn(1000))
 	}
 
 	fmt.Println(array[:10])
 
-	//сортировка
-	sort.Ints(array)
+	quickSort(array, 0, len(array)-1)
 
 	fmt.Println(array[:10])
 }
--- l1/17.go
+++ l1/17.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"sort"
 )
 
+func binSearch(array []int, l int, r int, key int) (int, error) {
+	for l != r {
+		mid := (l + r) / 2
+		if key <= array[mid] {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	if array[l] == key {
+		return l, nil
+	}
+	return -1, errors.New("no this value")
+}
+
 func main() {
-	array := make([]int, 20)
-	for i := 0; i < 20; i++ {
-		array[i] = rand.Intn(1000)
+	array := make([]int, 0, 20)
+	for i := 0; i < 10; i++ {
+		array = append(array, rand.Intn(1000))
 	}
 
 	//бин поиск
 	sort.Ints(array)
 	fmt.Println(array)
-	x := array[2]
-	i := sort.Search(len(array), func(i int) bool { return x <= array[i] })
+	x := 81
+	i, _ := binSearch(array, 0, len(array)-1, x)
 
 	fmt.Println(i)
 }
