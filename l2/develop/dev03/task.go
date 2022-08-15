52bc99953aef0e9524624700516578ebbc9eb98f
Artemiy <los28.2001@mail.ru>
2022-08-15 19:50:14 +0300

    L2 sort with commands

--- l2/develop/dev03/task.go
+++ l2/develop/dev03/task.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -21,23 +22,24 @@ func ListenCommand() {
 	str = strings.TrimSpace(str)
 	utility := "sort"
 	if strings.HasPrefix(str, utility) {
-		commandKeys := make(map[byte]struct{})
+		commandKeys := make(map[byte]int)
 		fileName := strings.Builder{}
 		for i := len(utility); i < len(str); i++ {
 			if str[i] == '-' {
 				i++
 				for str[i] != ' ' {
 					if str[i] == 'u' {
-						commandKeys['u'] = struct{}{}
+						commandKeys['u'] = 1
 					}
 					if str[i] == 'k' {
-						commandKeys['k'] = struct{}{}
+						i++
+						commandKeys['k'], _ = strconv.Atoi(string(str[i]))
 					}
 					if str[i] == 'n' {
-						commandKeys['n'] = struct{}{}
+						commandKeys['n'] = 1
 					}
 					if str[i] == 'r' {
-						commandKeys['r'] = struct{}{}
+						commandKeys['r'] = 1
 					}
 					i++
 				}
@@ -53,7 +55,56 @@ func ListenCommand() {
 	}
 }
 
-func sortUtil(fileName string, keys map[byte]struct{}) {
+type linesToSort struct {
+	lines []string
+	keys  map[byte]int
+}
+
+func (s linesToSort) Len() int {
+	return len(s.lines)
+}
+
+func (s linesToSort) Less(i, j int) bool {
+	str1 := s.lines[i]
+	str2 := s.lines[j]
+	if column, ok := s.keys['k']; ok {
+		flag1 := false
+		flag2 := false
+		if len(strings.Fields(str1)) < column {
+			flag1 = true
+		} else {
+			str1 = strings.Fields(str1)[column-1]
+		}
+		if len(strings.Fields(str2)) < column {
+			flag2 = true
+		} else {
+			str2 = strings.Fields(str2)[column-1]
+		}
+		if flag1 && !flag2 {
+			return true
+		} else if !flag1 && flag2 {
+			return false
+		}
+	}
+	if _, ok := s.keys['n']; ok {
+		str3, _ := strconv.Atoi(str1)
+		str4, _ := strconv.Atoi(str2)
+		if _, ok := s.keys['r']; ok {
+			return str3 > str4
+		}
+		return str3 < str4
+	}
+	if _, ok := s.keys['r']; ok {
+		return str1 > str2
+	}
+	return str1 < str2
+}
+
+func (s linesToSort) Swap(i, j int) {
+	s.lines[i], s.lines[j] = s.lines[j], s.lines[i]
+}
+
+func sortUtil(fileName string, keys map[byte]int) {
 	lines := make([]string, 0)
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -65,7 +116,15 @@ func sortUtil(fileName string, keys map[byte]struct{}) {
 	for sc.Scan() {
 		lines = append(lines, sc.Text())
 	}
-	sort.Strings(lines)
+	if _, ok := keys['u']; ok {
+		for i := 0; i < len(lines)-1; i++ {
+			if lines[i] == lines[i+1] {
+				lines = append(lines[0:i], lines[i+1:]...)
+				i--
+			}
+		}
+	}
+	sort.Sort(linesToSort{lines, keys})
 
 	writeFile, err := os.Create(fileName[0:len(fileName)-4] + "_sorted.txt")
 	defer writeFile.Close()
