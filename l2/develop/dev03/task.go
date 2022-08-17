40f065872967da1d4c09fac79f4a001250dfd3d3
Artemiy <los28.2001@mail.ru>
2022-08-17 16:59:13 +0300

    L2 unix-shell

--- l2/develop/dev03/task.go
+++ l2/develop/dev03/task.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"flag"
 	"log"
 	"os"
 	"sort"
@@ -10,54 +10,28 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Printf("Write you command here:\n")
-	myscanner := bufio.NewScanner(os.Stdin)
-	myscanner.Scan()
-	str := myscanner.Text()
-	str = strings.TrimSpace(str)
-	ListenCommand(str)
+type flags struct {
+	k int
+	n bool
+	r bool
+	u bool
 }
 
-func ListenCommand(str string) {
-	utility := "sort"
-	if strings.HasPrefix(str, utility) {
-		commandKeys := make(map[byte]int)
-		fileName := strings.Builder{}
-		for i := len(utility); i < len(str); i++ {
-			if str[i] == '-' {
-				i++
-				for str[i] != ' ' {
-					if str[i] == 'u' {
-						commandKeys['u'] = 1
-					}
-					if str[i] == 'k' {
-						i++
-						commandKeys['k'], _ = strconv.Atoi(string(str[i]))
-					}
-					if str[i] == 'n' {
-						commandKeys['n'] = 1
-					}
-					if str[i] == 'r' {
-						commandKeys['r'] = 1
-					}
-					i++
-				}
-			}
-			if str[i] != ' ' {
-				fileName.WriteRune(rune(str[i]))
-			}
-		}
-		if _, err := os.Stat(fileName.String()); os.IsNotExist(err) {
-			log.Fatal("file" + fileName.String() + " does not exist")
-		}
-		sortUtil(fileName.String(), commandKeys)
-	}
+func main() {
+
+	var fl flags
+	flag.IntVar(&fl.k, "k", 0, "указание колонки для сортировки")
+	flag.BoolVar(&fl.n, "n", false, "сортировать по числовому значению")
+	flag.BoolVar(&fl.r, "r", false, "сортировать в обратном порядке")
+	flag.BoolVar(&fl.u, "u", false, "не выводить повторяющиеся строки")
+	flag.Parse()
+	fileName := flag.Args()[1]
+	sortUtil(fileName, fl)
 }
 
 type linesToSort struct {
 	lines []string
-	keys  map[byte]int
+	keys  flags
 }
 
 func (s linesToSort) Len() int {
@@ -67,18 +41,18 @@ func (s linesToSort) Len() int {
 func (s linesToSort) Less(i, j int) bool {
 	str1 := s.lines[i]
 	str2 := s.lines[j]
-	if column, ok := s.keys['k']; ok {
+	if s.keys.k > 0 {
 		flag1 := false
 		flag2 := false
-		if len(strings.Fields(str1)) < column {
+		if len(strings.Fields(str1)) < s.keys.k {
 			flag1 = true
 		} else {
-			str1 = strings.Fields(str1)[column-1]
+			str1 = strings.Fields(str1)[s.keys.k-1]
 		}
-		if len(strings.Fields(str2)) < column {
+		if len(strings.Fields(str2)) < s.keys.k {
 			flag2 = true
 		} else {
-			str2 = strings.Fields(str2)[column-1]
+			str2 = strings.Fields(str2)[s.keys.k-1]
 		}
 		if flag1 && !flag2 {
 			return true
@@ -86,15 +60,15 @@ func (s linesToSort) Less(i, j int) bool {
 			return false
 		}
 	}
-	if _, ok := s.keys['n']; ok {
+	if s.keys.n {
 		str3, _ := strconv.Atoi(str1)
 		str4, _ := strconv.Atoi(str2)
-		if _, ok := s.keys['r']; ok {
+		if s.keys.r {
 			return str3 > str4
 		}
 		return str3 < str4
 	}
-	if _, ok := s.keys['r']; ok {
+	if s.keys.r {
 		return str1 > str2
 	}
 	return str1 < str2
@@ -104,7 +78,7 @@ func (s linesToSort) Swap(i, j int) {
 	s.lines[i], s.lines[j] = s.lines[j], s.lines[i]
 }
 
-func sortUtil(fileName string, keys map[byte]int) {
+func sortUtil(fileName string, fl flags) {
 	lines := make([]string, 0)
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -116,7 +90,7 @@ func sortUtil(fileName string, keys map[byte]int) {
 	for sc.Scan() {
 		lines = append(lines, sc.Text())
 	}
-	if _, ok := keys['u']; ok {
+	if fl.u {
 		for i := 0; i < len(lines)-1; i++ {
 			if lines[i] == lines[i+1] {
 				lines = append(lines[0:i], lines[i+1:]...)
@@ -124,7 +98,7 @@ func sortUtil(fileName string, keys map[byte]int) {
 			}
 		}
 	}
-	sort.Sort(linesToSort{lines, keys})
+	sort.Sort(linesToSort{lines, fl})
 
 	writeFile, err := os.Create(fileName[0:len(fileName)-4] + "_sorted.txt")
 	defer writeFile.Close()
--- l2/develop/dev08/task.go
+++ l2/develop/dev08/task.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
 
+func main() {
+	for {
+		var command string
+		fmt.Scan(&command)
+		switch command {
+		case "\\q":
+			os.Exit(0)
+		case "cd":
+			var argument string
+			_, err2 := fmt.Scan(&argument)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			err := os.Chdir(argument)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "pwd":
+			dir, _ := os.Getwd()
+			fmt.Println(dir)
+		case "echo":
+			var argument string
+			_, err2 := fmt.Scan(&argument)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			fmt.Print(argument)
+		case "kill":
+			var argument string
+			_, err2 := fmt.Scan(&argument)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			cmd := exec.Command("cmd", "/C", "taskkill /PID ", argument)
+			if err := cmd.Run(); err != nil {
+				log.Fatal(err)
+			}
+		case "ps":
+			cmd := exec.Command("cmd", "/C", "tasklist")
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print(string(out))
+		case "fork":
+			var procAttr os.ProcAttr
+			procAttr.Files = []*os.File{os.Stdin,
+				os.Stdout, os.Stderr}
+			_, err := os.StartProcess(os.Args[0], os.Args, &procAttr)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "exec":
+			sc := bufio.NewScanner(os.Stdin)
+			for sc.Scan() {
+				arguments := strings.Fields(sc.Text())
+				arguments = append([]string{"/C", "start"}, arguments...)
+				cmd := exec.Command("cmd.exe", arguments...)
+				out, err := cmd.CombinedOutput()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Print(string(out))
+			}
+		}
+
+	}
 }
